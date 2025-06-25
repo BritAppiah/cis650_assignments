f48d0876cb9ffaed064bf3424accfbe01671554a
BritAppiah <bappiahagyei@murraystate.edu>
2025-06-25 12:40:10 -0500

    Update 05_grocery_bill.py

--- statements/05_grocery_bill.py
+++ statements/05_grocery_bill.py
@@ -22,12 +22,13 @@ while items_count < num_items:
 
 print(seperator)
 print("Grocery Bill Summary")
+print(seperator)
 print(items_output)
 print(seperator)
 print(f"Total number of items: {items_count}")
 print(f"Total price: ${total_price:.2f}")
 print(seperator)
-print("Thank you for using the Grocery Bill Calculator")
+print("Thank you for using the Grocery Bill Calculator!")
 
 
 
