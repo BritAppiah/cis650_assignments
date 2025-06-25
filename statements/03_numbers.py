6d305091f41dd125143f513b4595e5e8860ba0c2
BritAppiah <bappiahagyei@murraystate.edu>
2025-06-25 10:19:20 -0500

    Update 03_numbers.py

--- statements/03_numbers.py
+++ statements/03_numbers.py
@@ -3,6 +3,13 @@
 # for example, if the user inputs 17, the system would print
 # 17, 34, 51, 68,.. 
 # Write your code below
-
+divisor = int(input('Enter positive integer to find multiples between 1 and 100: '))
+count = 0
+print(f"Numbers between 1 and 100 that are evenly divisible by {divisor}:")
+for number in range(1, 101):
+    if number % divisor == 0:
+        print(number, end=' ')
+        count += 1
+print(f"\nTotal numbers divisible by {divisor}: {count}") 
 
 # git push the changes with message '03_numbers'
