a9028c5fd0f04fbe684b2d022c98073318f1f046
BritAppiah <bappiahagyei@murraystate.edu>
2025-06-27 11:46:56 -0400

    Update 01_age_test.py

--- statements/01_age_test.py
+++ statements/01_age_test.py
@@ -12,7 +12,7 @@ elif age > 2000:
 elif age > 100:
     print('You are not Alice, grannie.')
 else:
-    print('You are not over the hill')
+    print('You are not over the hill.')
 
 #git commit -m 'updated 01_age_test' and then
 #git push
